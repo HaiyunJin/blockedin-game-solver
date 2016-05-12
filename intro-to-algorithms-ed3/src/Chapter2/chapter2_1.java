d4b1bcc8fe9d49bcc0982f91b9d79c07e9b0401c
HaiyunJin <jinhaiyun0101@gmail.com>
2016-05-12 13:12:00 -0500

    No Cment

--- intro-to-algorithms-ed3/src/Chapter2/chapter2_1.java
+++ intro-to-algorithms-ed3/src/Chapter2/chapter2_1.java
@@ -1,22 +1,27 @@
 package Chapter2;
 
+//import java.util.Arrays;
+
 public class chapter2_1 {
 
-		private boolean x;
-		
 		public static void main (String[] args) {
-		chapter2_1 foo = new chapter2_1();
-		System.out.println(foo.x);
-		
-		System.out.println(System.currentTimeMillis());
-		
+			System.out.println((int)(Math.random()*100));
+			
+//			Myarray myarray = new Myarray();
+			int[] array = new int[]{77, 32, 2, 26, 22, 21, 87, 45, 91, 30, 92, 42};
+			Myarray myarray = new Myarray(array);
+
+			myarray.insertionSort();
+			myarray.printOld();
+			myarray.printSorted();
+			
+	/*				
+		System.out.println(System.currentTim`eMillis());
 		java.util.Date date = new java.util.Date();
 		System.out.println("The elapsed time since Jan 1, 1970 is " +
 				date.getTime() + " millseconds.");
 		System.out.println(date.toString());
+	*/	
 		}
-		
-		
-
 }
 
